2260221da27322182f520f94e82302d9d339a915
ramesh <hari.kvana@gmail.com>
2016-09-15 18:32:52 +0530

    hardcoding commit

--- app/src/main/java/com/truevault_rest_api/activity/BaseActivity.java
+++ app/src/main/java/com/truevault_rest_api/activity/BaseActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.truevault_rest_api.R;
 import com.truevault_rest_api.networks.RetrofitHandler;
 import com.truevault_rest_api.util.Util;
 
@@ -31,55 +30,55 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_signin);
 
-//        final JSONObject userJson = new JSONObject();
-//        try {
-//            userJson.put("name", "Ramesh");
-//            userJson.put("email", "ramesh.kvana@gmail.com");
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
+        final JSONObject userJson = new JSONObject();
+        try {
+            userJson.put("name", "Ramesh");
+            userJson.put("email", "ramesh.kvana@gmail.com");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-//        RetrofitHandler.getInstance().createUser("ram12", "8982", Util.stringToBase64(userJson.toString()), true).enqueue(new Callback<String>() {
-//            @Override
-//            public void onResponse(Call<String> call, Response<String> response) {
-//                if (response.isSuccess()) {
-//                    Log.d(TAG, response.body());
-//                    try {
-//                        JSONObject jsonObject = new JSONObject(response.body());
-//                        JSONObject userJsonObject = jsonObject.optJSONObject("user");
-//                         user_id = userJsonObject.optString("user_id");
-
-
-//                        RetrofitHandler.getInstance().readUser(user_id).enqueue(new Callback<String>() {
-//
-//                            @Override
-//                            public void onResponse(Call<String> call, Response<String> response) {
-//                                if (response.isSuccess()) {
-//                                    Log.d(TAG, response.body());
-//                                } else
-//                                    Log.e(TAG, response.message());
-//                            }
-//
-//                            @Override
-//                            public void onFailure(Call<String> call, Throwable t) {
-//                                Log.d(TAG, t.getLocalizedMessage());
-//                            }
-//                        });
+        RetrofitHandler.getInstance().createUser("ram12", "8982", Util.stringToBase64(userJson.toString()), true).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccess()) {
+                    Log.d(TAG, response.body());
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body());
+                        JSONObject userJsonObject = jsonObject.optJSONObject("user");
+                         user_id = userJsonObject.optString("user_id");
 
 
-//                        RetrofitHandler.getInstance().updateUser("hari","1234",Util.stringToBase64(userJson.toString()),true).enqueue(new Callback<String>() {
-//                            @Override
-//                            public void onResponse(Call<String> call, Response<String> response) {
-//                                if (response.isSuccess()) {
-//                                    Log.d(TAG, response.body());
-//                                } else
-//                                    Log.e(TAG, response.message());
-//                            }
-//                            @Override
-//                            public void onFailure(Call<String> call, Throwable t) {
-//                                Log.d(TAG, t.getLocalizedMessage());
-//                            }
-//                        });
+                        RetrofitHandler.getInstance().readUser(user_id).enqueue(new Callback<String>() {
+
+                            @Override
+                            public void onResponse(Call<String> call, Response<String> response) {
+                                if (response.isSuccess()) {
+                                    Log.d(TAG, response.body());
+                                } else
+                                    Log.e(TAG, response.message());
+                            }
+
+                            @Override
+                            public void onFailure(Call<String> call, Throwable t) {
+                                Log.d(TAG, t.getLocalizedMessage());
+                            }
+                        });
+
+
+                        RetrofitHandler.getInstance().updateUser("hari","1234",Util.stringToBase64(userJson.toString()),true).enqueue(new Callback<String>() {
+                            @Override
+                            public void onResponse(Call<String> call, Response<String> response) {
+                                if (response.isSuccess()) {
+                                    Log.d(TAG, response.body());
+                                } else
+                                    Log.e(TAG, response.message());
+                            }
+                            @Override
+                            public void onFailure(Call<String> call, Throwable t) {
+                                Log.d(TAG, t.getLocalizedMessage());
+                            }
+                        });
 
 
                         RetrofitHandler.getInstance().deleteUser(user_id).enqueue(new Callback<String>() {
@@ -98,18 +97,18 @@ public class BaseActivity extends AppCompatActivity {
                         });
 
 
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                    }
-//                } else
-//                    Log.e(TAG, response.message());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<String> call, Throwable t) {
-//                Log.d(TAG, t.getLocalizedMessage());
-//            }
-//        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else
+                    Log.e(TAG, response.message());
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.d(TAG, t.getLocalizedMessage());
+            }
+        });
 
     }
     public void showProgressDialog(String message) {
